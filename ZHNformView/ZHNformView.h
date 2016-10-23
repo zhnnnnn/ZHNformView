e9e23bb2e45e4f88b23630d0d7b530181de57781
zhnnnnn <475816908@qq.com>
2016-10-23 20:09:38 +0800

    加了点点注释

--- ZHNformView/ZHNformView.h
+++ ZHNformView/ZHNformView.h
@@ -13,6 +13,13 @@
 - (NSInteger)numbOfSectionsInZHNformView:(ZHNformView *)forView;
 - (NSString *)ZHNformView:(ZHNformView *)formView ContentOfCol:(NSInteger)col inRow:(NSInteger)row;
 @optional
+
+/**
+ 返回列宽度百分比的数组（不实现这个方法默认是按照 headTitleArrayForZHNformView 方法返回的数组里面的文字长度的百分比来显示）
+
+ * 数组的里面的数字的和必须等于 1
+ * 数组里面的个数必须和 headTitleArrayForZHNformView 方法返回的数组的个数相等
+ */
 - (NSArray *)itemHeightPercentArrayForZHNformView:(ZHNformView *)formView;
 - (CGFloat)headTitleHeightForZHNformView:(ZHNformView *)formView;
 - (UIColor *)ZHNformView:(ZHNformView *)formView stringColorOfCol:(NSInteger)col inRow:(NSInteger)row;
@@ -29,23 +36,19 @@
 @interface ZHNformView : UIView
 
 @property (nonatomic,assign) CGFloat titleFont;
-
 @property (nonatomic,assign) CGFloat contentFont;
-
 @property (nonatomic,strong) UIColor * headTitleBackColor;
-
 @property (nonatomic,strong) UIColor * assistLayerColor;
 
+// 是否显示水平和垂直的辅助显示的view（不填默认是显示的）
 @property (nonatomic,getter = isShowVerticalLayer) BOOL showVerticalLayer;
-
 @property (nonatomic,getter = isShowHorizontalLayer) BOOL showHorizontalLayer;
 
 @property (nonatomic,weak) id <ZHNformViewDataSource> dataSource;
-
 @property (nonatomic,weak) id <ZHNformViewDelegate> delegate;
 
+// 刷新数据
 - (void)ZHN_reloadData;
-
+// 移除辅助layer
 - (void)ZHN_removeAssistLayer;
-
 @end
